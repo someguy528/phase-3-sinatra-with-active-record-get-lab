8356a2c944f5cf1a56cc52adbd3f3b6b80ada53c
Brandon Castro <castroyou999@gmail.com>
2023-01-26 22:52:57 -0500

    commit tree dab5e099c987384e752e8c3ef41134841088b860

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json' 
 
   # add routes
+  get "/bakeries" do
+    # Bakery.all.to_json
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get "/bakeries/:id" do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods )
+  end
+
+  get "/baked_goods/by_price" do
+    BakedGood.all.order(price: :desc).to_json
+  end
+
+  get "/baked_goods/most_expensive" do
+    BakedGood.all.order(price: :desc).first.to_json
+  end
 
 end
